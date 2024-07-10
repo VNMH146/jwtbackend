30496f94015b7730a795824f9edbdc67e15441c1
Huy <vohuy060214@gmail.com>
2024-07-10 18:09:08 +0700

    first init

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import configViewEngine from './configs/viewEngine';
+import initWebRoutes from './routes/web';
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+//config view engine
+configViewEngine(app);
+
+//init all web routes
+initWebRoutes(app);
+
+
+
+app.listen(PORT, () => {
+  console.log('Backend Nodejs is running on port: ' + PORT);
+})
