bb6f498d0c1444685fcf5777573aa3a007c77eef
Huy <vohuy060214@gmail.com>
2024-07-11 10:46:52 +0700

    update controller

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,11 +1,11 @@
 import express from 'express';
+import homeController from '../controller/homeController';
 
 const router = express.Router();
 
 const initWebRoutes = (app) => {
-  router.get('/', (req, res) => {
-    res.send("Hello World!")
-  })
+  router.get('/', homeController.handleHome);
+  router.get('/user', homeController.handleUserPage);
 
   return app.use("/", router);
 }
